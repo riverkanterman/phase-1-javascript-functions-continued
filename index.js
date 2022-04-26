e591705c312cc667d4be09c3df4941209f5c4eba
River <+>
2022-04-26 00:20:48 -0400

    commit tree 6cb5118f27b43d09982d0da1ed51be0315cc493a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork (workActivity = 'go to the office'){
+    return (`This Monday, I will ${workActivity}.`);
+}
+
+function wrapAdjective(param="*") {
+    const part1 = "You are"
+    return function innerFunction(param2="a hard worker") {
+return `${part1} ${param}${param2}${param}!`
+    };
+}
+
+wrapAdjective('||', 'a dedicated programmer')
